c0d4b16986d03ba819fede0a998631b6991fbb54
DanielIbrag <danny.ibragimov1@gmail.com>
2022-03-09 20:00:18 -0500

    if the first letter of the name starts with an A then it will have a different message

--- app/controllers/names_controller.rb
+++ app/controllers/names_controller.rb
@@ -1,6 +1,11 @@
 class NamesController < ApplicationController
   def name_params
-    input_name = params["names"]
-    render json: { message: input_name.upcase }
+    input_name = params["names"].upcase
+    split = input_name.split("")
+    if split[0] == "A"
+      render json: { message: "Hey, your name starts with the first letter of the alphabet" }
+    else
+      render json: { message: input_name }
+    end
   end
 end
